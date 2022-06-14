2d21c679f806f205ef3f5d061073d06f222492e6
tunggary <gsy1656@gmail.com>
2022-06-15 00:55:07 +0900

    한글 폰트 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "../styles/global.scss";
 import cookies from "next-cookies";
 import jwt from "jsonwebtoken";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 MyApp.getInitialProps = async ({ Component, ctx }) => {
--- pages/_document.js
+++ pages/_document.js
@@ -10,9 +10,6 @@ class MyDocument extends Document {
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" /> */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
