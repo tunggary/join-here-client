0cb73ee9d96f2e9d69ad7ec2dcd6e4586fa5d759
tunggary <gsy1656@gmail.com>
2022-10-10 00:57:33 +0900

    Add layout 컴포넌트

---
+++ pages/manage/applicant.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../../components/common/Layout";
+
+export default function Applicant({ loginInfo }) {
+  return (
+    <Layout loginInfo={loginInfo} pageTitle="지원자 관리">
+      sdfs
+    </Layout>
+  );
+}
