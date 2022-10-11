7c116c884dab87270a9f8da96e3a40b3ccd9a1f6
tunggary <gsy1656@gmail.com>
2022-10-12 00:22:10 +0900

    Fix mixed content error

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
           <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
           {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
