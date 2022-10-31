089b33b128965bee2c3a99f0637ea9988637bb86
tunggary <gsy1656@gmail.com>
2022-11-01 01:48:12 +0900

    Fix Input 컴포넌트 초기 로딩시 value 있으면 사이즈 맞춤

--- components/common/inputTemplate/Input.js
+++ components/common/inputTemplate/Input.js
@@ -1,9 +1,18 @@
 import styles from "@styles/components/common/inputTemplate.module.scss";
+import { useEffect } from "react";
+import { useRef } from "react";
 export default function Input({ id, label, value = "", name, readOnly = false, placeholder = "", onChange }) {
-  const autoResizeTextarea = ({ target }) => {
+  const ref = useRef(null);
+
+  const autoResizeTextarea = () => {
+    const target = ref.current;
     target.style.height = "auto";
     target.style.height = target.defaultValue.length > 20 ? `${target.scrollHeight + 8}px` : `56px`;
   };
+
+  useEffect(() => {
+    autoResizeTextarea();
+  }, []);
 
   return (
     <div className={styles.inputContainer}>
@@ -11,9 +20,11 @@ export default function Input({ id, label, value = "", name, readOnly = false, p
         {label}
       </label>
       {readOnly ? (
-        <div className={`${styles.input} ${styles.readBox}`}>{value}</div>
+        <div ref={ref} className={`${styles.input} ${styles.readBox}`}>
+          {value}
+        </div>
       ) : (
-        <textarea className={styles.textarea} id={id} name={name} defaultValue={value} placeholder={placeholder} onChange={onChange} onKeyDown={autoResizeTextarea} onKeyUp={autoResizeTextarea}></textarea>
+        <textarea ref={ref} className={styles.textarea} id={id} name={name} defaultValue={value} placeholder={placeholder} onChange={onChange} onKeyDown={autoResizeTextarea} onKeyUp={autoResizeTextarea}></textarea>
       )}
     </div>
   );
