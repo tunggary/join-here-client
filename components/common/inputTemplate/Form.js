bedfb7960893742d2c4ebb507f208022302ce7e8
tunggary <gsy1656@gmail.com>
2022-10-31 23:52:16 +0900

    Add onClick prop 추가

--- components/common/inputTemplate/Form.js
+++ components/common/inputTemplate/Form.js
@@ -1,9 +1,11 @@
 import styles from "@styles/components/common/inputTemplate.module.scss";
-export default function Form({ children, button = "확인" }) {
+export default function Form({ children, button = "확인", onClick }) {
   return (
     <div className={styles.container}>
       {children}
-      <button className={styles.submitButton}>{button}</button>
+      <button className={styles.submitButton} onClick={onClick}>
+        {button}
+      </button>
     </div>
   );
 }
