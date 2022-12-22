5c7796f2608878702ebbb8185870b0efd2105509
tunggary <gsy1656@gmail.com>
2022-12-22 16:25:34 +0900

    Refactor 쿠키 이름 변경

--- components/common/Header.js
+++ components/common/Header.js
@@ -15,7 +15,7 @@ export default function Header({ loginInfo, searchValue }) {
   };
 
   const logout = () => {
-    document.cookie = "id=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    document.cookie = "user_id=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     window.location.href = "/";
   };
 
