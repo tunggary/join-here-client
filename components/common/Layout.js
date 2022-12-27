3fce8a9afc763364e373413fc8467c797c4c95c8
tunggary <gsy1656@gmail.com>
2022-12-27 16:19:10 +0900

    Refactor Layout 컴포넌트 main 태그 제거

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,13 +1,13 @@
 import Header from "@components/common/Header";
 import styles from "@styles/components/common/Layout.module.scss";
 
-export default function Layout({ children, loginInfo, pageTitle = "" }) {
+export default function Layout({ children, pageTitle = "" }) {
   return (
     <div className={styles.container}>
-      <main className={styles.contentContainer}>
+      <div className={styles.contentContainer}>
         <h1 className={styles.pageTitle}>{pageTitle}</h1>
         <article className={styles.content}> {children}</article>
-      </main>
+      </div>
     </div>
   );
 }
