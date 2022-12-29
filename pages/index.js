53190c1e5376819d11e168e2db64ce7d2c1fc3da
tunggary <gsy1656@gmail.com>
2022-12-29 17:46:41 +0900

    Refactor 메인 페이지 PageWrapper 적용

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+import PageWrapper from "@components/common/PageWrapper";
 import Banner from "@components/home/Banner";
 import Category from "@components/home/Category";
 
 export default function Home() {
   return (
-    <>
+    <PageWrapper theme="white">
       <Banner />
       <Category />
-    </>
+    </PageWrapper>
   );
 }
