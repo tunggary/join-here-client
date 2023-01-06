57eeb7448fb7199ad657393eb71d3c07ee4dd0f4
tunggary <gsy1656@gmail.com>
2023-01-06 23:39:08 +0900

    Refactor 마이페이지 소속동아리 컴포넌트로 분리

--- pages/mypage/index.js
+++ pages/mypage/index.js
@@ -3,13 +3,13 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@styles/pages/mypage.module.scss";
-import Location from "@public/clublist/location.svg";
 import { dictClub, dictArea, mypageList, stateDict } from "@utils/util";
 import Arrow from "@public/manage/arrow-right.svg";
 import ssrWrapper from "@utils/wrapper";
 import axiosInstance from "@utils/axios";
 import PageWrapper from "@components/common/PageWrapper";
 import PersonalInfo from "@components/MyPage/PersonalInfo";
+import BelongClub from "@components/MyPage/BelongClub";
 
 export default function Home({ loginInfo, clubData, applicationData }) {
   const [index, setIndex] = useState(0);
@@ -30,33 +30,7 @@ export default function Home({ loginInfo, clubData, applicationData }) {
             <PersonalInfo userId={loginInfo.userName} />
           </TabPanel>
           <TabPanel>
-            <section className={styles.clubContainer}>
-              {clubData.map(({ belong: { club } }) => (
-                <div key={club.id} className={styles.club}>
-                  <div className={styles.poster}>
-                    <Image src={club.image || "/clublist/default.png"} alt="동아리 사진" width={115} height={163} />
-                  </div>
-                  <div className={styles.clubInfo}>
-                    <div className={styles.title}>{club.name}</div>
-                    <div className={styles.category}>{dictClub[club.category] ? dictClub[club.category][0] : club.category}</div>
-                    <div className={styles.location}>
-                      <Location />
-                      {dictArea[club.area]}
-                    </div>
-                    <div className={styles.desc}>{club.introduction}</div>
-                    <div className={styles.view}>
-                      <span>조회수 {club.view}</span> <span>찜한수 {club.scrap}</span>
-                    </div>
-                    <Link href={`/manage/${club.id}/member`}>
-                      <div className={styles.link}>
-                        동아리원 보기
-                        <Arrow />
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-              ))}
-            </section>
+            <BelongClub clubData={clubData} />
           </TabPanel>
           <TabPanel>
             <section className={styles.applicationContainer}>
