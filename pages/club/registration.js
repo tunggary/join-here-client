e7f1e19b08d7f951736ea1f8f3b000ad8751347d
tunggary <gsy1656@gmail.com>
2023-01-19 21:35:16 +0900

    Refactor 동아리 등록 submit form 수정

--- pages/club/registration.js
+++ pages/club/registration.js
@@ -2,32 +2,43 @@ import { useRouter } from "next/router";
 import ClubTemplate from "@components/common/Template/Club";
 import axiosInstance from "@utils/axios";
 import ssrWrapper from "@utils/wrapper";
-import { blobToBase64 } from "@utils/util";
+import { getFormData } from "@utils/util";
 import PageWrapper from "@components/common/PageWrapper";
 import TemplateWrapper from "@components/common/Template/TemplateWrapper";
 
 export default function Registration({ loginInfo }) {
   const router = useRouter();
 
+  const validationCheck = (value) => {
+    for (const key in value) {
+      if (key === "image" && value[key] === null) continue;
+      if (value[key] === "" || value[key] === null) {
+        alert("기본 정보를 입력해주세요");
+        return false;
+      }
+    }
+    return true;
+  };
+
   const onSubmit = async (value) => {
-    const { name, introduction, category, area, image } = value;
+    if (!validationCheck(value)) return;
 
-    await axiosInstance
-      .post("/clubs", {
-        id: loginInfo.userName,
-        name,
-        introduction,
-        category,
-        area,
-        image: image instanceof File ? await blobToBase64(image) : image,
-      })
-      .catch((err) => {
-        console.log(err.message);
-        alert("서버 오류가 발생했습니다. 잠시후 다시 시도해주세요");
-        return;
+    const formData = getFormData({
+      ...value,
+      id: loginInfo.userName,
+    });
+
+    try {
+      await axiosInstance.post("/clubs", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
       });
-
-    router.push("/manage");
+      alert("성공적으로 동아리를 등록했습니다.");
+      router.push("/manage");
+    } catch (error) {
+      alert("잠시후 다시 시도해주세요");
+    }
   };
 
   return (
